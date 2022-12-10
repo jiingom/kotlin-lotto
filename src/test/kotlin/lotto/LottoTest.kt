699e4ae0c1e6254a6d043a04c446a143a73db82f
jiingom <vvankatam0@gmail.com>
2022-12-10 23:42:52 +0900

    test(Lotto): 로또 예외 처리 테스트

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,7 @@
 package lotto
 
+import lotto.domain.Lotto
+import lotto.domain.WinningLotto
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -21,4 +23,10 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `로또 번호와 보너스 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            WinningLotto(Lotto(listOf(1, 2, 3, 4, 5, 5)), 1)
+        }
+    }
 }
